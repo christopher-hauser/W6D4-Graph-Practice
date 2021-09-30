746ef9f6e1774c7f2a4cefee8645963086a6993d
Max <maxtgrigg@gmail.com>
2021-09-30 14:19:10 -0700

    cheif

--- island.js
+++ island.js
@@ -1,18 +1,44 @@
 function getNeighbors(row, col, graph) {
 
   // Check top
-
-  // Check bottom
+  let neighbors = [];
+  if (row > 0) {
+    if (graph[row - 1][col] === 1) {
+      neighbors.push([row - 1, col])
+    }
+  }
+  if (row < graph.length - 1) {
+    if (graph[row + 1][col] === 1) {
+      neighbors.push([row + 1, col])
+    }
+  }
 
   // Check left
-
+  if (col > 0) {
+    if (graph[row][col - 1] === 1) {
+      neighbors.push([row, col - 1])
+    }
+  }
   // Check right
-
+  if (col < graph[0].length - 1) {
+    if (graph[row][col + 1] === 1) {
+      neighbors.push([row, col + 1])
+    }
+  }
   // Return neighbors
-
+ // console.log(neighbors
+    
+  return neighbors
   // Your code here
 }
+// graph= [
+//   [1,1,1,0,0],
+//   [0,1,1,0,1],
+//   [0,1,1,0,1],
+// ]
 
+// getNeighbors(0, 0, graph);  // 7
+// getNeighbors(2, 4, graph);
 
 function islandSize(row, col, graph) {
 
@@ -26,14 +52,14 @@ function islandSize(row, col, graph) {
 
   // While the stack is not empty,
 
-    // Pop the first node
+  // Pop the first node
 
-    // DO THE THING (increment size by 1)
+  // DO THE THING (increment size by 1)
 
-    // Then push all the UNVISITED neighbors on top of the stack
-    // and mark them as visited
-    // HINT: This is what your helper function `getNeighbors` is for
-    // HINT: Remember, you're storing your visited nodes as strings!
+  // Then push all the UNVISITED neighbors on top of the stack
+  // and mark them as visited
+  // HINT: This is what your helper function `getNeighbors` is for
+  // HINT: Remember, you're storing your visited nodes as strings!
 
   // return size
 
